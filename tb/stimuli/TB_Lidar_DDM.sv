4a3398925b50c303e60fc743480129e31ea4d5a2
alinazmeu <zmeu.ali@gmail.com>
2025-05-01 17:23:54 +0200

    Create TB_Lidar_DDM.sv

---
+++ tb/stimuli/TB_Lidar_DDM.sv
@@ -0,0 +1,72 @@
+
+`timescale 1ns/1ps
+
+module TB_Lidar_DDM ();
+
+logic clk_i, rstn_i, ready_ACM_i, ready_CCM_i, valid_data_i, testmode_i;
+logic valid_datapoint_DDM, valid_azimuth_DDM;
+logic [15:0] azimuth_DDM_o, distance_o;
+logic [7:0] data_i;
+logic [3:0] id_o;
+
+
+logic [18:0] [7:0] data = {8'hFF, 8'hEE, 8'h0, 8'h7, 8'h3, 8'h99, 8'h0, 8'h9, 8'h69, 8'h0, 8'h4, 8'h40, 8'h0,  8'h9, 8'h6F, 8'h0, 8'h5, 8'h2E, 8'h0 }; 
+
+
+
+Lidar_DDM DUT(
+.clk_i(clk_i),
+.rstn_i(rstn_i),
+.ready_ACM_i(ready_ACM_i),
+.ready_CCM_i(ready_CCM_i),
+.valid_data_i(valid_data_i),
+.data_i(data_i),
+.testmode_i(testmode_i),
+.valid_datapoint_DDM_o(valid_data_point_DDM),
+.valid_azimuth_DDM_o(valid_azimuth_DDM),
+.azimuth_DDM_o(azimuth_DDM_o),
+.distance_o(distance_o),
+.id_o(id_o)
+);
+
+int num_sim=0;
+int length_sim=20;
+int pointer=18;
+
+initial begin
+	clk_i='0;
+	while(1) begin
+		#10;
+		clk_i=~clk_i;
+	end
+end
+
+initial begin
+	rstn_i='0;
+	#5;
+	rstn_i=1'b1;
+	
+end
+
+
+initial begin
+testmode_i=1'b1;
+ready_CCM_i=1'b0;
+ready_ACM_i=1'b0;
+valid_data_i=1'b0;
+wait(rstn_i)
+ready_CCM_i=1'b1;
+ready_ACM_i=1'b1;
+
+while(num_sim<=length_sim) begin
+@(posedge clk_i);
+valid_data_i=1'b1;
+data_i=data[pointer];
+pointer--;
+num_sim++;
+end
+
+$stop;
+end
+
+endmodule
