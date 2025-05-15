6f46aa0087ed03df01cbfea3672ad18e0ddfa628
alinazmeu <zmeu.ali@gmail.com>
2025-05-15 12:04:41 +0200

    Update write_out_pkg.sv

--- tb/write_out_pkg.sv
+++ tb/write_out_pkg.sv
@@ -45,4 +45,19 @@ task write_z_out(input logic signed[17:0] z[$], string path);
     $display("z outputs written correctly to sim/%s", path);
 endtask
 
+task write_serial_out(input logic signed[15:0] serial[$], string path);
+    int file;
+
+    file = $fopen(path, "w");
+    if(file == 0)
+        $fatal(1, "Error: Could not create sim/%s!", path);
+
+    foreach(serial[i]) begin
+        $fwrite(file, "%d\n", serial[i]);
+    end
+
+    $fclose(file);
+    $display("z outputs written correctly to sim/%s", path);
+endtask
+
 endpackage
