36041ab630b4918d201932f433bb63366841df91
alinazmeu <zmeu.ali@gmail.com>
2025-05-15 12:02:37 +0200

    Update TB_Lidar_top_level.sv

--- tb/TB_Lidar_top_level.sv
+++ tb/TB_Lidar_top_level.sv
@@ -5,37 +5,46 @@ module TB_Lidar_top_level();
 
 import load_stim_pkg::*;
 import write_out_pkg::*;
+import axi_stream_test::*;
 
 logic clk_i;
 logic rstn_i;
-logic testmode_i;
+
+//Input AXI-S (from framing top)
 logic valid_data_i;
 logic [7:0] data_i;
-logic valid_datapoint_CCM_o;
 logic ready_DDM_o;
-logic signed [17:0] x_o, y_o, z_o;
+
+//Output AXI-S (to upsizer)
+logic valid_serial_o;
+logic signed [15:0] data_o;
+logic ready_upsizer_i;
+
+//control logic signals 
 logic [7:0] nr_packets;
 
-	// Create interface instance
+
+/*****AXIS-S INJECTION ******/
+
+  //AXI interface instance
   AXI_STREAM_BUS_DV #(.DataWidth(8)) axi_if (.clk_i(clk_i));
 
-  // Virtual interface to pass to class
-virtual AXI_STREAM_BUS_DV #(.DataWidth(8)) v_axi_if = axi_if;
+  // AXI Virtual interface instance 
+  virtual AXI_STREAM_BUS_DV #(.DataWidth(8)) v_axi_if = axi_if;
 
-//instance od driver object in my tb
+//instance of stream_driver class from stream_test package
 axi_stream_test::axi_stream_driver #(.DataWidth(8)) axi_tx;
 
+//DEVICE UNDER TEST
 Lidar_top_level DUT(
 	.rstn_i(rstn_i),
 	.clk_i (clk_i),
 	.data_i(axi_if.tdata),
 	.valid_data_i(axi_if.tvalid),
-	.valid_datapoint_CCM_o(valid_datapoint_CCM_o),
-	.x_o(x_o),
-	.y_o(y_o),
-	.z_o(z_o),
 	.ready_DDM_o(axi_if.tready),
-	.testmode_i(testmode_i),
+	.valid_serial_o(valid_serial_o),
+	.ready_upsizer_i(ready_upsizer_i),
+	.data_o(data_o),
 	.nr_packets(nr_packets)
 );
 
@@ -49,8 +58,7 @@ logic [7:0] bytes[$];
 assign length_sim = $bits(bytes)/8;
 
 // Declare output vectors
-logic signed [17:0] x[$], y[$], z[$];
-
+logic signed [15:0] serial[$];
 
 // Load stimuli from .txt files
 initial begin
@@ -69,39 +77,60 @@ initial begin
 	rstn_i='0;
 	#5;
 	rstn_i=1'b1;
+	
 end
 
+
+
+
 initial begin
-testmode_i=1'b1;
 ready_upsizer_i=1'b1;
-//valid_data_i=1'b0;
-axi_tx=new(v_axi_if);
+
+axi_tx=new(v_axi_if); //virtual interface is passed to the driver 
 axi_tx.reset_tx();
+
 wait(rstn_i)
 @(posedge clk_i);
 while(index<=length_sim) begin
 axi_tx.send(bytes[index], 1'b0);
-data_i.tdata=bytes[index];
 
-if(valid_datapoint_CCM_o) begin
-	x[out_idx] = x_o;
-	y[out_idx] = y_o;
-	z[out_idx] = z_o;
-	//$display("x=%d", x[out_idx] );
-
+if(valid_serial_o) begin
+	serial[out_idx] = data_o;
 	out_idx++;
-end 
+end
+
 index++;
 end
 
-write_x_out(x, "x_out.txt");
-write_y_out(y, "y_out.txt");
-write_z_out(z, "z_out.txt");
+write_serial_out(serial, "serial_out_txt");
 $display("Number of packets: %d", nr_packets);
 
 repeat(5) @(posedge clk_i);
 
 $stop;
 end
+
+/****PARALLEL OUTPUT****/
+
+
+// Declare output vectors
+/* logic signed [17:0] x[$], y[$], z[$]; */
+
+//in while loop
+/* if(valid_datapoint_CCM_o) begin
+	x[out_idx] = x_o;
+	y[out_idx] = y_o;
+	z[out_idx] = z_o;
+	//$display("x=%d", x[out_idx] );
+
+	out_idx++;
+end */  
+
+
+//out of the loop
+/* write_x_out(x, "x_out.txt");
+write_y_out(y, "y_out.txt");
+write_z_out(z, "z_out.txt"); */
+
 
 endmodule
